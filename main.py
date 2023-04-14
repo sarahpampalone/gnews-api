07b9d0da01e5d0da50221ec978d726eabd0f7204
sarahpampalone <120332370+sarahpampalone@users.noreply.github.com>
2023-04-14 19:08:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,19 @@ print("   2. Search by keyword")
 print("   3. Search by title")
 
 stop = 0
+stop2 = 0
 while(stop != 1):
     choice1 = input("Number: ")
 
     match choice1:
         case "1":
-            number = input("How many articles would you like to see? (1-10): ")
-            choice2 = input("Would you like to choose a category? Y/N: ")
+            while (stop2 != 1):
+                number = input("How many articles would you like to see? (1-10): ")
+                if int(number)>0 & int(number)<11 :
+                    stop2 = 1
+                else:
+                    print("Invalid input. Try again!")
+            choice2 = input("Would you like to choose a category? (Y/N): ")
             if choice2 == "Y":
                 print("What category would you like?")
                 print("   1. General")
@@ -34,7 +40,13 @@ while(stop != 1):
             stop = 1;
         case "2":
             key = input("What keyword would you like to search? ")
-            keyword(key)
+            while (stop2 != 1):
+                num = input("How many articles would you like to see? (1-10): ")
+                if int(num)>0 & int(num)<11 :
+                    stop2 = 1
+                else:
+                    print("Invalid input. Try again!")
+            keyword(key,num)
             stop = 1;
         case "3":
             print("What is the title of the article? (Check your spelling!)")
