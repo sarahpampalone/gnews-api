9103ccb392a9c03f4d1a24742577b7c3b51f70e1
sarahpampalone <120332370+sarahpampalone@users.noreply.github.com>
2023-04-14 18:56:54 -0400

    Update title.py

--- title.py
+++ title.py
@@ -2,15 +2,10 @@ from functools import cache
 
 @cache
 def title(t):
-    # https://docs.python.org/3/library/json.html
-    # This library will be used to parse the JSON data returned by the API.
-    import json
-    # https://docs.python.org/3/library/urllib.request.html#module-urllib.request
-    # This library will be used to fetch the API.
-    import urllib.request
+    import json   # This library will be used to parse the JSON data returned by the API.
+    import urllib.request   # This library will be used to fetch the API.
 
     apikey = "79c76560c3197501d56bf153b6bf5eb1"
-    #t = 'Google Chat rolling out Material You web redesign'
     search = t.replace(" ", "+" )
     url = f"https://gnews.io/api/v4/search?q={search}&in=title&lang=en&country=us&max=10&apikey={apikey}"
 
@@ -19,13 +14,6 @@ def title(t):
         articles = data["articles"]
 
         for i in range(len(articles)):
-            # articles[i].title
-            print(f"\n{articles[i]['title']}")
-            # articles[i].description
-            print(f"{articles[i]['description']}")
-            # You can replace {property} below with any of the article properties returned by the API.
-            # articles[i].{property}
-            # print(f"{articles[i]['{property}']}")
-
-            # Delete this line to display all the articles returned by the request. Currently only the first article is displayed.
+            print(f"\n{i+1}. {articles[i]['title']}")   # articles[i].title
+            print(f"{articles[i]['description']}")   # articles[i].description
             break
