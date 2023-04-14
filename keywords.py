de028be6946afc1945c37578364a7d9eee99548a
sarahpampalone <120332370+sarahpampalone@users.noreply.github.com>
2023-04-14 18:49:37 -0400

    Update keywords.py

--- keywords.py
+++ keywords.py
@@ -14,13 +14,6 @@ def keyword(key, num):
         articles = data["articles"]
 
         for i in range(len(articles)):
-            # articles[i].title
-            print(f"\n{articles[i]['title']}")
-            # articles[i].description
-            print(f"{articles[i]['description']}")
-            # You can replace {property} below with any of the article properties returned by the API.
-            # articles[i].{property}
-            # print(f"{articles[i]['{property}']}")
-
-            # Delete this line to display all the articles returned by the request. Currently only the first article is displayed.
-            break
+            print(f"\n{articles[i]['title']}")   # articles[i].title
+            print(f"{articles[i]['description']}")   # articles[i].description
+            # You can replace {property} below with any of the article properties returned by the API
